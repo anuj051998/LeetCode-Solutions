e61d918a67a93737a05b59d7754643df49921159
Anuj Vishwakarma <rv219392@gmail.com>
2022-07-20 13:00:35 +0530

    check

---
+++ LeetCode/Solutions/Regular Expression Matching.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LeetCode.Solutions
+{
+    public partial class Solution
+    {
+        public bool IsMatch(string s, string p)
+        {
+            return Regex.Match(s, p).Success;
+        }
+    }
+}
